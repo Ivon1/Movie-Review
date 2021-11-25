6052c73c921d84051a7a736a49b8b124fe74625a
Ivon-cpp <vania9023@gmail.com>
2021-11-25 16:17:28 +0200

    Update v3

---
+++ Movie Review/Data/Migrations/20211121213701_Updated to 230MaxSizeDescription.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Movie_Review.Data.Migrations
+{
+    public partial class Updatedto230MaxSizeDescription : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "MovieDescription",
+                table: "Film",
+                type: "nvarchar(230)",
+                maxLength: 230,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(240)",
+                oldMaxLength: 240);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "MovieDescription",
+                table: "Film",
+                type: "nvarchar(240)",
+                maxLength: 240,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(230)",
+                oldMaxLength: 230);
+        }
+    }
+}
